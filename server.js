41453f9f420a93e7800156ee073d0cbb684fe0a5
AcedYu <alexyu299@hotmail.com>
2021-04-20 12:01:45 -0700

    moved server.js to root directory

--- server.js
+++ server.js
@@ -13,17 +13,17 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Sets up the express app to be able to access this folder (specifically used to get the CSS for one of the pages)
-app.use(express.static(path.join(__dirname, '../public')));
+app.use(express.static(path.join(__dirname, '/public')));
 
 // Default navigates to index.html
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../public/index.html'));
+  res.sendFile(path.join(__dirname, '/public/index.html'));
 });
 
 // get request for notes navigates to notes.html
 app.get('/notes', (req, res) => {
-  res.sendFile(path.join(__dirname, '../public/notes.html'));
+  res.sendFile(path.join(__dirname, '/public/notes.html'));
 });
 
 // Initialize app
-app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
+app.listen(PORT, () => console.log(`App listening on http://localhost:${PORT}`));
