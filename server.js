2d308b7cfd99306bfd5fb26f6043238d9a4e045e
AcedYu <acedyu@DESKTOP-F4GSCBI.localdomain>
2021-04-21 19:26:11 -0700

    complete

--- server.js
+++ server.js
@@ -6,7 +6,13 @@ const fs = require('fs');
 // define express app
 const app = express();
 // define port
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+
+var notes;
+fs.readFile(path.join(__dirname, '/db/db.json'), (err, data) => {
+  if (err) throw err;
+  notes = JSON.parse(data);
+});
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
@@ -23,6 +29,48 @@ app.get('/', (req, res) => {
 // get request for notes navigates to notes.html
 app.get('/notes', (req, res) => {
   res.sendFile(path.join(__dirname, '/public/notes.html'));
+});
+
+app.get('/api/notes', (req, res) => {
+  res.json(notes);
+});
+
+app.post('/api/notes', (req, res) => {
+  var newNote = req.body;
+  newNote.id = notes.length + 1;
+  notes.push(newNote);
+
+  fs.writeFile(path.join(__dirname, '/db/db.json'), JSON.stringify(notes), err => {
+    if (err) {
+      console.log(`Error creating file. Error message of: ${err}`);
+    }
+  });
+
+  res.json(newNote);
+});
+
+app.delete('/api/notes/:id', (req, res) => {
+  var id = req.params.id;
+  var newNotes = [];
+  for (let i = 0; i < notes.length; i++) {
+    if (id == notes[i].id) {
+      continue;
+    }
+    newNotes.push(notes[i]);
+  }
+
+  notes = newNotes;
+
+  for (let i = 0; i < notes.length; i++) {
+    notes[i].id = i + 1;
+  }
+
+  fs.writeFile(path.join(__dirname, '/db/db.json'), JSON.stringify(notes), err => {
+    if (err) {
+      console.log(`Error creating file. Error message of: ${err}`);
+    }
+  });
+  res.json(notes);
 });
 
 // Initialize app
